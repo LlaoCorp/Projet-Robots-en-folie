58ec5ab2f12332d9bb5ebf681af1062e7da4a8f0
Valentin <valentin.llaomoles@gmail.com>
2025-06-06 16:47:50 +0200

    Ajout des fonctions de déplacement et nettoyage du code

--- controle_robot/fonctions_motrices.py
+++ controle_robot/fonctions_motrices.py
@@ -1,4 +1,13 @@
-def suivre_ligne():
+import time, hcsr04, roues
+from machine import Pin, PWM
+
+# Définition des composants
+mes_roues = roues.Roues(14,27,26,25)
+capteur_gauche = Pin(15, Pin.IN)
+capteur_droite = Pin(4, Pin.IN)
+servo = PWM(Pin(13), freq=50)
+
+def suivre_ligne(capteur_gauche, capteur_droite):
     if capteur_gauche.value() != 0 and capteur_droite.value() != 0:
         print("stop")
     elif capteur_gauche.value() == 0 and capteur_droite.value() == 0:
@@ -7,6 +16,18 @@ def suivre_ligne():
         print("aller légèrement à droite") # genre droite->wait->avance
     else:
         print("aller légèrement à gauche") # genre gauche->wait->avance
+
+def distanceMesure():
+    pulsor = hcsr04.HCSR04(5,18)
+    distance = pulsor.distance_cm()
+    return distance
+
+def set_angle(angle):
+    # Convertit l'angle (0-180) en rapport cyclique (duty)
+    min_duty = 26  # correspond à ~0.5ms
+    max_duty = 128  # correspond à ~2.5ms
+    duty = int(min_duty + (angle / 180) * (max_duty - min_duty))
+    servo.duty(duty)
 
 # FONCTIONS DU CUBE
 def attraper_cube():
--- controle_robot/main.py
+++ controle_robot/main.py
@@ -3,11 +3,6 @@ import time, hcsr04, roues, fonctions_motrices
 from machine import Pin, PWM
 from fonctions_motrices import *
 
-def distanceMesure():
-    pulsor = hcsr04.HCSR04(5,18)
-    distance = pulsor.distance_cm()
-    return distance
-
 def test_roues():
     try:
         # mes_roues.avancer()
@@ -34,21 +29,9 @@ def test_pwm():
     in1 = PWM(Pin(26), freq=500, duty=500)
     in2 = PWM(Pin(27), freq=500, duty=500)
 
-def set_angle(angle):
-    # Convertit l'angle (0-180) en rapport cyclique (duty)
-    min_duty = 26  # correspond à ~0.5ms
-    max_duty = 128  # correspond à ~2.5ms
-    duty = int(min_duty + (angle / 180) * (max_duty - min_duty))
-    servo.duty(duty)
-
-# Définition des composants
-mes_roues = roues.Roues(14,27,26,25)
-capteur_gauche = Pin(15, Pin.IN)
-capteur_droite = Pin(4, Pin.IN)
-servo = PWM(Pin(13), freq=50)
-
 # Début de la boucle
 while True:
     print(distanceMesure())
     time.sleep(1)
-    suivre_ligne()
+    suivre_ligne(capteur_gauche, capteur_droite)
+    cherche_cube()
